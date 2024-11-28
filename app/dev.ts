645626987515adfcc4fa877bfd7ac26ff12c5329
devkush0999 <devesh2cse@gmail.com>
2024-11-28 23:15:43 +0530

    .

---
+++ app/dev.ts
@@ -0,0 +1,19 @@
+let numArray: [number, string] = [1, "One"];
+numArray.push(0);
+let numArray1: [number] = [1, 1, 6, 7, 3];
+numArray1.forEach(val => val.);
+
+
+
+let obj: { name: string, age: number } = {
+    name: "John",
+    age: 30
+}
+
+obj = { name: "Jane" };
+
+
+
+let a = 10
+
+
